50f8e95d0bcdb02adcf5499a1a5647f612324587
Odintsov Sergei (loveravel) <loveravel@yandex.ru>
2019-09-26 02:32:31 +0400

    init: completed the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if (!str.length || str.length % 2 !== 0) {
+        return false;
+    }
+
+    let i = 0;
+
+    while (i < bracketsConfig.length) {
+        const currentConfig = bracketsConfig[i].join('');
+        if (str.includes(currentConfig)) {
+            str = str.replace(currentConfig, '');
+            i = 0;
+        } else {
+            i++;
+        }
+    }
+
+    return !str;
 }
